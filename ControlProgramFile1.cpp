3d443bbc3f868da6b985ec3e14b649b3812f03e0
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-03-09 16:49:59 +0700

    menambahkan cout file 1

--- ControlProgramFile1.cpp
+++ ControlProgramFile1.cpp
@@ -5,10 +5,12 @@ int main(){
     int x;
 
     srand(time(0));
-    x = rand () % 10;
+    x = rand() % 10;
 
     if (x==5){
         x = x+1;
-        
+        cout<<"nilai x sudah dikoreksi menjadi : "<<x<<endl;
     }
- 
+
+    cout << "Bilangan yang diberikan = "<<x;
+}
