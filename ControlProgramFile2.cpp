28d888e0879e387c5b49f9ee85d90cc2bb98ab32
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-03-09 16:59:43 +0700

    menambahkan string status lulus

--- ControlProgramFile2.cpp
+++ ControlProgramFile2.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 float Rata_rata(float a, float b){
     return (a+b)/2;
-    
+
+    string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos"; 
+    else
+        return "Gagal";
+}
+
+string Status_lulus2(float r){
+    if (r >= 60)
+        return "Lolos"; 
+    else
+        return "Gagal";
+}
+
 }
